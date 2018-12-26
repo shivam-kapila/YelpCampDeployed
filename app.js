d7eb2bdfac68b7a406b9273acc264a445c8c76a4
shivam-kapila <shivamkapila4@gmail.com>
2018-12-26 13:07:44 +0000

    add mongolab

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost/yelp_camp_v11deployed");
-mongoose.connect("mongodb://"+process.env.DB_USER+":"+process.env.DB_USER_PASS+"@ds143594.mlab.com:43594/yelpcamp");
+mongoose.connect("mongodb://shivamkapila:shivamkapila99@ds143594.mlab.com:43594/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
